9dc4fb44068cf88bddcf75e074123106ac45bca3
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2021-05-31 18:20:22 +0400

    project

--- src/containers/routes/shop-page/index.js
+++ src/containers/routes/shop-page/index.js
@@ -1,24 +1,38 @@
-import React, {Component} from 'react';
-import ComponenctA from "./componenctA";
-import ComponenctB from "./componenctB";
+import React, {Component, useState, useEffect} from 'react';
 
 class Shop extends Component {
     state = {
-        countA: 0,
-        countB: 0,
-        countC: 0,
+        background: ''
     }
+
+    a = () => {
+        this.setState({
+            background: 'green'
+        })
+    };
+    b = () => {
+        this.setState({
+            background: 'red'
+        })
+    };
+    c = () => {
+        this.setState({
+            background: 'black'
+        })
+    }
+
 
     render() {
         return (
-            <div>
-                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-                    <button onClick={() => this.setState({countA: 1})}>Change CountA</button>
-                    <button onClick={() => this.setState({countB: 1})}>Change CountB</button>
-                    <button onClick={() => this.setState({countC: 1})}>Change CountC</button>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+                <div>
+                    <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '200px'}}>
+                        <button onClick={this.a}>GREEN</button>
+                        <button onClick={this.b}>RED</button>
+                        <button onClick={this.c}>BLACK</button>
+                    </div>
+                    <div style={{width: '200px', height: '200px', border: '1px solid black', background: this.state.background}}/>
                 </div>
-                <ComponenctA countA={this.state.countA}/>
-                <ComponenctB countB={this.state.countB} countC={this.state.countC}/>
             </div>
         );
     }
